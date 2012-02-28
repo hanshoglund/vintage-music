7febcfae07c2e26560ac14845df59ac90cf1192a
Hans Höglund <hans@hanshoglund.se>
2012-02-28 01:41:47 +0100

    - Added Music.Rendering.Graphics.Diagrams

--- src/Music/Render/Graphics/Diagrams.hs
+++ src/Music/Render/Graphics/Diagrams.hs
@@ -8,3 +8,36 @@
 -}
 
 module Music.Render.Graphics.Diagrams where
+
+{-# LANGUAGE 
+    NoMonomorphismRestriction #-}
+
+import Diagrams.Prelude hiding (width, height)
+import Diagrams.Backend.Cairo
+
+import Music.Utilities
+
+
+
+
+
+
+
+
+
+-- 
+-- Imperative test stuff
+--
+
+writeGraphics :: FilePath -> Diagram Cairo R2 -> IO ()
+writeGraphics file diagram = do 
+    fst $ renderDia Cairo ( CairoOptions file $ PDF (500, 500) ) diagram
+    return ()
+
+example :: Diagram Cairo R2                        
+example = circle 1 <> centerX (text "hans")
+
+main = do
+    writeGraphics "test.pdf" example
+    openFile "test.pdf"
+    return ()
