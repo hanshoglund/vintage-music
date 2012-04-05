f3dd6efb839d2f0349d29863326eab56ccf562d1
Hans Höglund <hans@hanshoglund.se>
2012-04-05 02:26:36 +0200

    - Beaming module

---
+++ src-notab/Notable/Beaming.hs
@@ -0,0 +1,16 @@
+
+{-# LANGUAGE
+    FlexibleContexts #-}
+
+module Notable.Beaming
+(
+)
+
+where
+
+import Notable.Core
+import Notable.Core.Diagrams
+import Notable.Core.Symbols                                                             
+
+import qualified Data.List as List
+
